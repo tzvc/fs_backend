c6099098818509d9ffaa2a88654673a82dcc0150
theochampion <contact.theochampion@gmail.com>
2019-10-02 13:15:52 +0200

    :construction: Added game module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ServerModule } from './server/server.module';
 import { UsersModule } from './users/users.module';
+import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [UsersModule, ServerModule],
+  imports: [UsersModule, ServerModule, GameModule],
   controllers: [AppController],
   providers: [],
 })
