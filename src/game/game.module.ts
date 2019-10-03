4c0d71239e813dbda0c48d96e84b75575781576c
theochampion <contact.theochampion@gmail.com>
2019-10-03 16:47:17 +0200

    :construction: Added game control gateway

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { GameGateway } from './game.gateway';
+import { UsersModule } from '../users/users.module';
+import { GameService } from './game.service';
 
-@Module({})
+@Module({
+  imports: [],
+  providers: [GameGateway, GameService],
+  exports: [],
+})
 export class GameModule {}
